c4ff3995976503e046bc6a7e246570b4b936e52a
Marcilio Farias <marcilio.m.farias@gmail.com>
2023-09-15 21:34:36 +1200

    foudation

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nodets-petshop
+Project module node
+
+# Global request
+`npm i -g nodemon typescript ts-node`
+
+# Instalation
+`npm i`
+
+# Run 
+`npm start`
---
+++ src/controllers/pageController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+export const home = (req:Request, res:Response)=> {
+
+    //res.render('page/home);
+};
+
+export const dogs = (req:Request, res:Response)=> {
+
+    //res.render('page/dogs);
+};
+
+export const cats = (req:Request, res:Response)=> {
+
+    //res.render('page/cats);
+};
+
+export const fishes = (req:Request, res:Response)=> {
+
+    //res.render('page/fishes);
+};
